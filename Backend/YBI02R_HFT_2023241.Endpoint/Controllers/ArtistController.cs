70522149d6f179d0ac255a4f5a1776fc08e7c3be
Vinyooo <contact.mrvinyo@gmail.com>
2023-12-02 13:30:14 +0100

    working on endpoint
    https://nikprog.hu/courses/halado-fejlesztesi-technikak/lecke/07-feladat-endpoint-reteg-fejlesztese/

---
+++ Backend/YBI02R_HFT_2023241.Endpoint/Controllers/ArtistController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using YBI02R_HFT_2023241.Logic;
+using YBI02R_HFT_2023241.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YBI02R_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+
+        IArtistLogic logic;
+
+        public ArtistController(IArtistLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        #region CRUD
+        // GET: api/<ArtistController>
+        [HttpGet]
+        public IEnumerable<Artist> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+        // GET api/<ArtistController>/5
+        [HttpGet("{id}")]
+        public Artist Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+        // POST api/<ArtistController>
+        [HttpPost]
+        public void Create([FromBody] Artist value)
+        {
+            this.logic.Create(value);
+        }
+        // PUT api/<ArtistController>/5
+        [HttpPut("{id}")]
+        public void Update([FromBody] Artist id)
+        {
+            this.logic.Update(id);
+        }
+        // DELETE api/<ArtistController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+        #endregion
+    }
+}
--- Backend/YBI02R_HFT_2023241.Endpoint/Startup.cs
+++ Backend/YBI02R_HFT_2023241.Endpoint/Startup.cs
@@ -3,19 +3,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using YBI02R_HFT_2023241.Logic.Interfaces;
+using YBI02R_HFT_2023241.Logic;
+using YBI02R_HFT_2023241.Models;
+using YBI02R_HFT_2023241.Repository.Database;
+using YBI02R_HFT_2023241.Repository.Interfaces;
+using YBI02R_HFT_2023241.Repository.Repositories.ModelRepos;
 
 namespace YBI02R_HFT_2023241.Endpoint
 {
     public class Startup
     {
+
+        public Startup()
+        {
+               
+        }
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<MusicDbContext>();
+            services.AddTransient<IRepository<Song>, SongRepo>();
+            services.AddTransient<IRepository<Artist>, ArtistRepo>();
+            services.AddTransient<IRepository<Publisher>, PublisherRepo>();
+            services.AddTransient<ISongLogic, SongLogic>();
+            services.AddTransient<IArtistLogic, ArtistLogic>();
+            services.AddTransient<IPublisherLogic, PublisherLogic>();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
