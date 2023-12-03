074597c1456b8cc866ef21d66898acb16dd4bb13
Vinyooo <contact.mrvinyo@gmail.com>
2023-12-03 21:33:04 +0100

    added most popular artist

--- Backend/YBI02R_HFT_2023241.Logic/Classes/StatLogic.cs
+++ Backend/YBI02R_HFT_2023241.Logic/Classes/StatLogic.cs
@@ -39,12 +39,34 @@ namespace YBI02R_HFT_2023241.Logic.Classes
             return artistRepo.ReadAll().SelectMany(x => x.Songs).OrderByDescending(x => x.Length).FirstOrDefault();
         }
 
+        public double? MinutesListenedToPublisher(string publisherName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Artist MostPopularArtist()
+        {
+            List<(Artist Artist, int SumOfPlays)> artistScore = new List<(Artist, int)>();
+            foreach (var artist in artistRepo.ReadAll())
+            {
+                int plays = 0;
+                foreach (var song in artist.Songs)
+                {
+                    plays += song.Plays;
+                }
+                artistScore.Add((artist, plays));
+            }
+            return artistScore.OrderByDescending(x => x.SumOfPlays).FirstOrDefault().Artist;
+        }
+
+        public Song MostPopularSongOfArtist(string artistName)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public int? OldestArtistAge()
         {
             return this.artistRepo.ReadAll().Max(x => x.Age);
         }
-
-
-
     }
 }
--- Backend/YBI02R_HFT_2023241.Logic/Interfaces/IStatLogic.cs
+++ Backend/YBI02R_HFT_2023241.Logic/Interfaces/IStatLogic.cs
@@ -9,6 +9,8 @@ namespace YBI02R_HFT_2023241.Logic.Interfaces
         Song LongestSong();
         Artist ArtistWithMostSongs();
         List<double?> AvgSongLengthForArtist();
-
+        Artist MostPopularArtist(); //gets the artist with most plays
+        Song MostPopularSongOfArtist(string artistName);
+        double? MinutesListenedToPublisher(string publisherName); //gets the publisher's artist(s) and returns with the artists songs plays * songlengths /60
     }
 }
