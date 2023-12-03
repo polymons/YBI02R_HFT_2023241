981f7e69b0385b271d4132d52a2668f2a46cb8ee
Vinyooo <contact.mrvinyo@gmail.com>
2023-12-03 17:31:53 +0100

    setup testing

--- Backend/YBI02R_HFT_2023241.Test/Test.cs
+++ Backend/YBI02R_HFT_2023241.Test/Test.cs
@@ -1,12 +1,32 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
+using YBI02R_HFT_2023241.Logic.Classes;
+using YBI02R_HFT_2023241.Models;
+using YBI02R_HFT_2023241.Repository.Interfaces;
 
 namespace YBI02R_HFT_2023241.Test
 {
     [TestFixture]
     internal class Test
     {
+        ArtistLogic artistLogic;
+        Mock<IRepository<Artist>> artistRepoMock;
+        PublisherLogic publisherLogic;
+        Mock<IRepository<Publisher>> publisherRepoMock;
+        SongLogic songLogic;
+        Mock<IRepository<Song>> songRepoMock;
 
+        [SetUp]
+        public void Init()
+        {
+            artistRepoMock = new Mock<IRepository<Artist>>();
+
+
+
+
+
+        }
 
     }
 }
