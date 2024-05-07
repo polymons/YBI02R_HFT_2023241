3fc03daab4858f3cdfface2af007976e729cb80d
Vince <42940948+polymons@users.noreply.github.com>
2024-05-08 00:43:38 +0200

    Revert "Delete Frontend/YBI02R_HFT_2023241.WpfClient directory"
    This reverts commit 15a8e93d8ef5f4e2ce563c4231f9827f4ec2afb2.

---
+++ Frontend/YBI02R_HFT_2023241.WpfClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace YBI02R_HFT_2023241.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Frontend/YBI02R_HFT_2023241.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace YBI02R_HFT_2023241.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
