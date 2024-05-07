7d5cd7c4459e03aa39eebb5d6f9cbe8cadc251da
Vince <42940948+polymons@users.noreply.github.com>
2024-05-07 21:55:21 +0200

    fix crud

--- Frontend/YBI02R_HFT_2023241.JSClient/wwwroot/Scripts/artist.js
+++ Frontend/YBI02R_HFT_2023241.JSClient/wwwroot/Scripts/artist.js
@@ -14,135 +14,136 @@ getData();
 setupSignalR();
 
 async function getData() {
-  await fetch("http://localhost:53910/artist")
-    .then((x) => x.json())
-    .then((y) => {
-      display = y;
-      console.log(y);
-      displayItems();
-    });
+	await fetch("http://localhost:53910/artist")
+		.then((x) => x.json())
+		.then((y) => {
+			display = y;
+			console.log(y);
+			displayItems();
+		});
 }
 function setupSignalR() {
-  connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:53910/hub")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+	connection = new signalR.HubConnectionBuilder()
+		.withUrl("http://localhost:53910/hub")
+		.configureLogging(signalR.LogLevel.Information)
+		.build();
 
-  connection.on("ArtistCreated", (user, message) => {
-    getData();
-  });
+	connection.on("ArtistCreated", (user, message) => {
+		getData();
+	});
 
-  connection.on("ArtistDeleted", (user, message) => {
-    getData();
-  });
+	connection.on("ArtistDeleted", (user, message) => {
+		getData();
+	});
 
-  connection.on("ArtistUpdated", (user, message) => {
-    getData();
-  });
+	connection.on("ArtistUpdated", (user, message) => {
+		getData();
+	});
 
-  connection.onclose(async () => {
-    await start();
-  });
-  start();
+	connection.onclose(async () => {
+		await start();
+	});
+	start();
 }
 async function start() {
-  try {
-    await connection.start();
-    console.log("SignalR Connected");
-  } catch (err) {
-    console.log(err);
-    setTimeout(start, 5000);
-  }
+	try {
+		await connection.start();
+		console.log("SignalR Connected");
+	} catch (err) {
+		console.log(err);
+		setTimeout(start, 5000);
+	}
 }
 function displayItems() {
-  itemDisplayElement.innerHTML = null;
-  display.forEach((t) => {
-    itemDisplayElement.innerHTML +=
-      "<tr><td>" +
-      t.artistID +
-      "</td><td>" +
-      t.name +
-      "</td><td>" +
-      t.age +
-      "</td><td><button class='btn btn-success' type='button' onclick='deleteItem(" +
-      t.artistID +
-      ")'>Delete</button><button class='btn btn-success' type='button' onclick='showUpdate(" +
-      t.artistID +
-      ")'>Update</button></td></tr>";
-  });
+	itemDisplayElement.innerHTML = null;
+	display.forEach((t) => {
+		itemDisplayElement.innerHTML +=
+			"<tr><td>" +
+			t.artistID +
+			"</td><td>" +
+			t.name +
+			"</td><td>" +
+			t.age +
+			"</td><td><button class='btn btn-success' type='button' onclick='deleteItem(" +
+			t.artistID +
+			")'>Delete</button><button class='btn btn-success' type='button' onclick='showUpdate(" +
+			t.artistID +
+			")'>Update</button></td></tr>";
+	});
 }
 function deleteItem(artistID) {
-  fetch("http://localhost:53910/artist/" + artistID, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: null,
-  })
-    .then((response) => response)
-    .then((data) => {
-      console.log("Success: ", data);
-      getData();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	fetch("http://localhost:53910/artist/" + artistID, {
+		method: "DELETE",
+		headers: { "Content-Type": "application/json" },
+		body: null,
+	})
+		.then((response) => response)
+		.then((data) => {
+			console.log("Success: ", data);
+			getData();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 }
 
 function create() {
-  let artistID = Number(IdElement.value);
-  let name = NameElement.value;
-  let age = Number(AgeElement.value);
+	let artistID = Number(IdElement.value);
+	let name = NameElement.value;
+	let age = Number(AgeElement.value);
 
-  fetch("http://localhost:53910/artist/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ artistID: artistID, name: name, age: age }),
-  })
-    .then((response) => response)
-    .then((data) => {
-      console.log("Success: ", data);
-      getData();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	fetch("http://localhost:53910/artist/", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ artistID: artistID, name: name, age: age }),
+	})
+		.then((response) => response)
+		.then((data) => {
+			console.log("Success: ", data);
+			getData();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 
-  IdElement.value = null;
-  NameElement.value = null;
-  AgeElement.value = null;
+	IdElement.value = null;
+	NameElement.value = null;
+	AgeElement.value = null;
 }
 
 function showUpdate(artistID) {
-  var toBeUpdated = display.find((d) => d["artistID"] == artistID);
-  document.getElementById("updateDiv").style.display = null;
-  document.getElementById("formDiv").style.display = "none";
+	var toBeUpdated = display.find((d) => d["artistID"] == artistID);
+	document.getElementById("updateDiv").style.display = null;
+	document.getElementById("formDiv").style.display = "none";
 
-  updateIdElement.value = toBeUpdated.artistID;
-  updateNameElement.value = toBeUpdated.name;
-  updateAgeElement.value = toBeUpdated.age;
+	updateIdElement.value = toBeUpdated.artistID;
+	updateNameElement.value = toBeUpdated.name;
+	updateAgeElement.value = toBeUpdated.age;
 }
 
 function update() {
-  document.getElementById("updateDiv").style.display = "none";
-  document.getElementById("formDiv").style.display = null;
-  let artistID = Number(updateIdElement.value);
-  let name = updateNameElement.value;
-  let age = Number(updateAgeElement.value);
+	document.getElementById("updateDiv").style.display = "none";
+	document.getElementById("formDiv").style.display = null;
+	let artistID = Number(updateIdElement.value);
+	let name = updateNameElement.value;
+	let age = Number(updateAgeElement.value);
 
-  fetch("http://localhost:53910/artist/", {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ artistID: artistID, name: name, age: age }),
-  })
-    .then((response) => response)
-    .then((data) => {
-      console.log("Success: ", data);
-      getData();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+	fetch("http://localhost:53910/artist/", {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ artistID: artistID, name: name, age: age }),
+	})
+		.then((response) => response)
+		.then((data) => {
+			console.log("Success: ", data);
+			getData();
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 
-  updateIdElement.value = null;
-  updateNameElement.value = null;
-  updateAgeElement.value = null;
+	updateIdElement.value = null;
+	updateNameElement.value = null;
+	updateAgeElement.value = null;
 }
+
--- Frontend/YBI02R_HFT_2023241.JSClient/wwwroot/Scripts/song.js
+++ Frontend/YBI02R_HFT_2023241.JSClient/wwwroot/Scripts/song.js
@@ -1,145 +1,169 @@
 let display = [];
 let connection = null;
-const itemDisplayElement = document.getElementById('displayItems');
+const itemDisplayElement = document.getElementById("displayItems");
 
-const IdElement = document.getElementById('itemId');
-const NameElement = document.getElementById('itemName');
-const BirthElement = document.getElementById('itemBirth');
-const WeightElement = document.getElementById('itemWeight');
-const ColorElement = document.getElementById('itemColor');
+const IdElement = document.getElementById("songID");
+const TitleElement = document.getElementById("Title");
+const GenreElement = document.getElementById("Genre");
+const ArtistElement = document.getElementById("Artist");
+const PlaysElement = document.getElementById("Plays");
 
-const updateIdElement = document.getElementById('updateId');
-const updateNameElement = document.getElementById('updateName');
-const updateBirthElement = document.getElementById('updateBirth');
-const updateWeightElement = document.getElementById('updateWeight');
-const updateColorElement = document.getElementById('updateColor');
+const updateIdElement = document.getElementById("updateId");
+const updateTitleElement = document.getElementById("updateTitle");
+const updateGenreElement = document.getElementById("updateGenre");
+const updateArtistElement = document.getElementById("updateArtist");
+const updatePlaysElement = document.getElementById("updatePlays");
 
 getData();
 setupSignalR();
 
 async function getData() {
-    await fetch('http://localhost:53910/Song')
-        .then(x => x.json())
-        .then(y => {
-            display = y;
-            console.log(y);
-            displayItems();
-        });
+  await fetch("http://localhost:53910/Song")
+    .then((x) => x.json())
+    .then((y) => {
+      display = y;
+      console.log(y);
+      displayItems();
+    });
 }
 function setupSignalR() {
-    connection = new signalR.HubConnectionBuilder()
-        .withUrl("http://localhost:53910/hub")
-        .configureLogging(signalR.LogLevel.Information)
-        .build()
+  connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:53910/hub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
 
-    connection.on("SongCreated", (user, message) => {
-        getData();
-    });
+  connection.on("SongCreated", (user, message) => {
+    getData();
+  });
 
-    connection.on("SongDeleted", (user, message) => {
-        getData();
-    });
+  connection.on("SongDeleted", (user, message) => {
+    getData();
+  });
 
-    connection.on("SongUpdated", (user, message) => {
-        getData();
-    });
+  connection.on("SongUpdated", (user, message) => {
+    getData();
+  });
 
-    connection.onclose(async () => {
-        await start();
-    });
-    start();
+  connection.onclose(async () => {
+    await start();
+  });
+  start();
 }
 async function start() {
-    try {
-        await connection.start();
-        console.log("SignalR Connected");
-    } catch (err) {
-        console.log(err);
-        setTimeout(start, 5000);
-    }
+  try {
+    await connection.start();
+    console.log("SignalR Connected");
+  } catch (err) {
+    console.log(err);
+    setTimeout(start, 5000);
+  }
 }
 function displayItems() {
-    itemDisplayElement.innerHTML = null;
-    display.forEach(t => {
-        itemDisplayElement.innerHTML += "<tr><td>" + t.id + "</td><td>" + t.name + "</td><td>" + t.birthYear + "</td><td>" + t.weight + "</td><td>" + t.color + "</td><td><button class='btn btn-success' type='button' onclick='deleteItem(" + t.id + ")'>Delete</button><button class='btn btn-success' type='button' onclick='showUpdate(" + t.id + ")'>Update</button></td></tr>"
-    });
+  itemDisplayElement.innerHTML = null;
+  display.forEach((t) => {
+    itemDisplayElement.innerHTML +=
+      "<tr><td>" +
+      t.songID +
+      "</td><td>" +
+      t.title +
+      "</td><td>" +
+      t.genre +
+      "</td><td>" +
+      t.plays +
+      "</td><td><button class='btn btn-success' type='button' onclick='deleteItem(" +
+      t.songID +
+      ")'>Delete</button><button class='btn btn-success' type='button' onclick='showUpdate(" +
+      t.songID +
+      ")'>Update</button></td></tr>";
+  });
 }
-function deleteItem(id) {
-    fetch('http://localhost:53910/Song/' + id, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json', },
-        body: null,
+function deleteItem(songID) {
+  fetch("http://localhost:53910/Song/" + songID, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: null,
+  })
+    .then((response) => response)
+    .then((data) => {
+      console.log("Success: ", data);
+      getData();
     })
-        .then(response => response)
-        .then(data => {
-            console.log('Success: ', data);
-            getData();
-        })
-        .catch((error) => { console.error('Error:', error) });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function create() {
-    let id = Number(IdElement.value);
-    let name = NameElement.value;
-    let birth = Number(BirthElement.value);
-    let weigth = Number(WeightElement.value);
-    let color = Number(ColorElement.value);
+  let songID = Number(IdElement.value);
+  let title = TitleElement.value;
+  let genre = GenreElement.value;
+  let plays = Number(PlaysElement.value);
 
-    fetch('http://localhost:53910/Song', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify({ id: id, name: name, birthYear: birth, weight: weigth, color: color })
+  fetch("http://localhost:53910/Song/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      songID: songID,
+      title: title,
+      genre: genre,
+      plays: plays,
+    }),
+  })
+    .then((response) => response)
+    .then((data) => {
+      console.log("Success: ", data);
+      getData();
     })
-        .then(response => response)
-        .then(data => {
-            console.log('Success: ', data);
-            getData();
-        })
-        .catch((error) => { console.error('Error:', error) });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 
-    IdElement.value = null;
-    NameElement.value = null;
-    BirthElement.value = null;
-    WeightElement.value = null;
-    ColorElement.value = null;
+  IdElement.value = null;
+  TitleElement.value = null;
+  GenreElement.value = null;
+  PlaysElement.value = null;
 }
 
-function showUpdate(id) {
-    var toBeUpdated = display.find(d => d['id'] == id);
-    document.getElementById("updateDiv").style.display = null;
-    document.getElementById("formDiv").style.display = "none";
+function showUpdate(songID) {
+  var toBeUpdated = display.find((d) => d["songID"] == songID);
+  document.getElementById("updateDiv").style.display = null;
+  document.getElementById("formDiv").style.display = "none";
 
-    updateIdElement.value = toBeUpdated.id;
-    updateNameElement.value = toBeUpdated.name;
-    updateBirthElement.value = toBeUpdated.birthYear;
-    updateWeightElement.value = toBeUpdated.weight;
-    updateColorElement.value = toBeUpdated.color;
+  updateIdElement.value = toBeUpdated.songID;
+  updateTitleElement.value = toBeUpdated.title;
+  updateGenreElement.value = toBeUpdated.genre;
+  updatePlaysElement.value = toBeUpdated.plays;
 }
 
 function update() {
-    document.getElementById("updateDiv").style.display = "none";
-    document.getElementById("formDiv").style.display = null;
-    let id = Number(updateIdElement.value);
-    let name = updateNameElement.value;
-    let birth = Number(updateBirthElement.value);
-    let weigth = Number(updateWeightElement.value);
-    let color = Number(updateColorElement.value);
+  document.getElementById("updateDiv").style.display = "none";
+  document.getElementById("formDiv").style.display = null;
+  let songID = Number(updateIdElement.value);
+  let title = updateTitleElement.value;
+  let genre = updateGenreElement.value;
+  let plays = Number(updatePlaysElement.value);
 
-    fetch('http://localhost:53910/Song', {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify({ id: id, name: name, birthYear: birth, weight: weigth, color: color })
+  fetch("http://localhost:53910/Song/", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      songID: songID,
+      title: title,
+      genre: genre,
+      plays: plays,
+    }),
+  })
+    .then((response) => response)
+    .then((data) => {
+      console.log("Success: ", data);
+      getData();
     })
-        .then(response => response)
-        .then(data => {
-            console.log('Success: ', data);
-            getData();
-        })
-        .catch((error) => { console.error('Error:', error) });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 
-    updateIdElement.value = null;
-    updateNameElement.value = null;
-    updateBirthElement.value = null;
-    updateWeightElement.value = null;
-    updateColorElement.value = null;
+  updateIdElement.value = null;
+  updateTitleElement.value = null;
+  updateGenreElement.value = null;
+  updatePlaysElement.value = null;
 }
